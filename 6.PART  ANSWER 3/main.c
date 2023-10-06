6218d6e5407f81f4b32b379fb72069061f9fc4f6
ENES DEDEOĞLU <101185956+enesdedeoglu0@users.noreply.github.com>
2023-10-06 16:17:43 +0300

    Add files via upload

---
+++ 6.PART  ANSWER 3/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc , char ** argv ) {
+    int a = 5;
+    int b;
+
+    b =++a + a++ * --a;
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
+    return     0;
+}
